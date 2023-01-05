b901c2b9864c23cf133283265a38d63fc77f73ba
Howie.Lau <microhaohao@outlook.com>
2023-01-05 14:03:48 +0800

    Add leetcode-452: Minimum Number of Arrows To Burst Balloons.

--- leetcode/include/utils.h
+++ leetcode/include/utils.h
@@ -6,4 +6,10 @@
 #define convert_arr2vec(arr, type) \
   std::vector<type>(arr, arr + sizeof(arr) / sizeof(type))
 
+static __inline__ long long get_time_ns() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000000000LL + ts.tv_nsec;
+}
+
 #endif // PUZZLES_LEETCODE_UTILS_H
